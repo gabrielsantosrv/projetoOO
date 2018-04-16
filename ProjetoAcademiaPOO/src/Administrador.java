56639ab23d240102629d0dfe927ffbd116c54a04
Grandinho <lucascdeo@gmail.com>
2018-04-16 18:34:53 -0300

    Esqueleto Admin e User

---
+++ ProjetoAcademiaPOO/src/Administrador.java
@@ -0,0 +1,30 @@
+public class Administrador extends Usuario{
+
+    public Administrador(String nome, String endereco, String telefone, String dataNascimento, boolean ativo, String username, String senha) {
+        super(nome, endereco, telefone, dataNascimento, ativo, username, senha);
+    }
+
+    public void incluirInstrutor(){
+
+    }
+
+    public void excluirInstrutor(){
+
+    }
+
+    public void alterarInstrutor(){
+
+    }
+
+    public void incluirUsuario(){
+
+    }
+
+    public void excluirUsuario(){
+
+    }
+
+    public void alterarUsuario(){
+
+    }
+}
---
+++ ProjetoAcademiaPOO/src/Usuario.java
@@ -0,0 +1,71 @@
+public class Usuario extends Pessoa{
+
+    private String username;
+    private String senha;
+
+    public Usuario(String nome, String endereco, String telefone, String dataNascimento, boolean ativo, String username, String senha) {
+        super(nome, endereco, telefone, dataNascimento, ativo);
+        this.username = username;
+        this.senha = senha;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void incluirCliente(){
+
+    }
+
+    public void excluirCliente(){
+
+    }
+
+    public void alterarCliente(){
+
+    }
+
+    public void incluirAtividade(){
+
+    }
+
+    public void excluirAtividade(){
+
+    }
+
+    public void alterarAtividade(){
+
+    }
+
+    public void incluirTurma(){
+
+    }
+
+    public void excluirTurma(){
+
+    }
+
+    public void alterarTurma(){
+
+    }
+
+    public void relacionarTurmaAtividade(){
+
+    }
+
+    public void void relacionarTurmaCliente(){
+
+    }
+}
