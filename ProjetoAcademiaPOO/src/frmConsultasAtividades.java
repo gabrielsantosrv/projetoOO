b5fb4f1f474e3535a52c1af9f6abf6c6aad8d8c1
Ana Clara Zoppi Serpa <anaclara.zoppiserpa@gmail.com>
2018-05-17 17:27:06 +0000

    Telas

---
+++ ProjetoAcademiaPOO/src/frmConsultasAtividades.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class frmConsultasAtividades {
+    private JPanel panel1;
+    private JTextField atividadeComMaisClientesTextField;
+    private JTextField atividadeComMenosClientesTextField;
+    private JTextField atividadeComMaiorPrecoTextField;
+    private JTextField atividadeComMenorPrecoTextField;
+    private JList listaDeAtividadesList;
+    private JLabel atividadeComMaisClientesLabel;
+    private JLabel atividadeComMenosClientesLabel;
+    private JLabel atividadeComMaiorPrecoLabel;
+    private JLabel atividadeComMenorPrecoLabel;
+    private JLabel listaDeAtividadesLabel;
+}
