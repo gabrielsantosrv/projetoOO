5e712f7319afbc0d988ff6141e61753ec132a198
Bruno de Marco <brunodemarco@hotmail.com.br>
2018-06-20 19:32:42 -0300

    txtfueldehu

--- ProjetoAcademiaPOO/src/frmDadosUsuario.java
+++ ProjetoAcademiaPOO/src/frmDadosUsuario.java
@@ -5,6 +5,8 @@ import java.awt.event.MouseEvent;
 public class frmDadosUsuario {
     private JComboBox comboBox1;
     private JButton salvarButton;
+    private JTextField txtUsuario;
+    private JTextField txtSenha;
 
     public frmDadosUsuario() {
         salvarButton.addMouseListener(new MouseAdapter() {
