c5fdadf1c76ed8126b0b93140f1817f3e287408e
Bruno de Marco <brunodemarco@hotmail.com.br>
2018-06-23 23:52:36 -0300

    alteracoes

--- ProjetoAcademiaPOO/src/frmDadosInstrutor.java
+++ ProjetoAcademiaPOO/src/frmDadosInstrutor.java
@@ -12,17 +12,39 @@ public class frmDadosInstrutor {
     private JTextField txtSalario;
     private JButton salvarButton;
     private JTextArea listaAreas;
+    private JComboBox comboTipo;
 
     public frmDadosInstrutor() {
         salvarButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
                 super.mouseClicked(mouseEvent);
+                String nome = txtNome.getText();
+                String rg = txtRg.getText();
+                String nascimento = txtNascimento.getText();
+                String telefone = txtTelefone.getText();
+                String endereco = txtEndereco.getText();
+                String salario = txtSalario.getText();
+                String[] areas = listaAreas.getText().split(";");
+                String tipoInstrutor = (String) comboTipo.getSelectedItem();
+                if(!nome.isEmpty() && !rg.isEmpty() && !nascimento.isEmpty() && !telefone.isEmpty() && !endereco.isEmpty() && !salario.isEmpty() && areas.length != 0 && !tipoInstrutor.isEmpty()){
+                    switch (tipoInstrutor){
+                        case "Diário":
+                            Gerenciador.incluirInstrutorDiario(Gerenciador.getLoginAtual(), rg, nome, endereco, telefone, nascimento, Float.valueOf(salario), areas);
+
+                        break;
+
+                    }
+                }
             }
         });
     }
 
     public JPanel getTelaDadosInstrutor() {
         return telaDadosInstrutor;
+    }
+
+    public void populaAreasNaLista(){
+
     }
 }
