5ae8cde98610f10b5b95d9a2605d4d8350078ca6
Bruno de Marco <brunodemarco@hotmail.com.br>
2018-06-26 19:24:13 -0300

    tela personal

---
+++ ProjetoAcademiaPOO/src/frmAssociaPersonal.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class frmAssociaPersonal {
+    private JTextField txtCliente;
+    private JTextField txtPersonal;
+    private JButton associarButton;
+
+    public frmAssociaPersonal() {
+
+        associarButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                super.mouseClicked(mouseEvent);
+            }
+        });
+    }
+}
