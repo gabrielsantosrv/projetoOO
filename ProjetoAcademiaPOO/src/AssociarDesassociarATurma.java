6017202fb8ebf8fd2692ad1453d74478fa880888
grandinho <lucascdeo@gmail.com>
2018-06-26 19:26:22 -0300

    associando

--- ProjetoAcademiaPOO/src/AssociarDesassociarATurma.java
+++ ProjetoAcademiaPOO/src/AssociarDesassociarATurma.java
@@ -15,6 +15,11 @@ public class AssociarDesassociarATurma {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
                 super.mouseClicked(mouseEvent);
+                String rg = txtRg.getText();
+                String atividade = txtTurma.getText();
+                String horario = txtHorario.getText();
+                Gerenciador.relacionarTurmaCliente(rg, atividade, horario);
+                Gerenciador.relacionarTurmaInstrutor(rg, atividade, horario);
             }
         });
     }
